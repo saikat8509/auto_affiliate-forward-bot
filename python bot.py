5a38a3f9645996130f2d3d3ed7ac3ade5571fbf7
saikat8509 <mailmetosaikat007@gmail.com>
2025-05-01 10:43:32 +0530

    Create python bot.py

---
+++ python bot.py
@@ -0,0 +1,10 @@
+
+## Deploy to Koyeb
+
+- Add your repo to Koyeb
+- Set the command: `python bot.py`
+- Add your environment variables or hardcode them in `config.py`
+
+## Author
+
+Made by [@saikat8509](https://github.com/saikat8509)
