9f03dcebca7c6495f1c76b88f7d18e161ef0f2af
saikat8509 <mailmetosaikat007@gmail.com>
2025-05-01 11:25:46 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+API_ID = 23584757         # Replace with your API ID
+API_HASH = "ac9926d2cb8acc38413f5e93881fd514"  # Replace with your API hash
+SESSION_STRING = "BQFn3_UArltv-ds0HB2_DkzjBRVKETAMK82g3qlNJlHQyPZa-PBWTmY4N8VJvqksgLCb_ej0BtIOCrUfCsXP_9oaMNDsuG4EwtCWB7U9w6DMIQZlsYe8HDx8YK4BVfi_q_vXTUwd3GsCsLW6PS0eacfDB-Dh6-QcU0UaUkzJ7bJabZUYvKvuY4WKbdh8RrZllUNh_wHvB8F8aJ4fVWywkd6LsEbcfbskS5ttGu7vAZnYrhKyrTbJ4YIEVYzmodz2Lx1_oXM1l4IhqVIlRro7bOc1yhsae2ir7OdTp4v9-3tL_OJi1fpip8ZWm4VTdCiy129y2W09ERJA4UoCiNzCxQc6t8sFTwAAAAHYYT3_AQ"  # Generate using Pyrogram string session
+
+SOURCE_CHANNELS = ['@sourcechannel1', '@sourcechannel2']
+DEST_CHANNELS = ['@mychannel1', '@mychannel2']
+
+REMOVE_KEYWORDS = ["unwantedword", "remove_this_link"]
+CUSTOM_CAPTION = "🔥 Grab the deal now! #BestDeals"
